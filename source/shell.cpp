acf09927cc9d1562867313fbebcb83dda0dc92e3
Tareq Alazzam <tareqalazzam6@gmail.com>
2025-08-29 06:06:47 +0300

    repl

--- source/shell.cpp
+++ source/shell.cpp
@@ -34,7 +34,7 @@ void Shell::handleCommand(const std::string& input) {
     return; 
   } else if (cmd == "type") { 
      std::string s; iss>>s;
-      if(builtins.find(builtins.begin(), builtins.end(), s) != builtins.end())
+      if(std::find(builtins.begin(), builtins.end(), s) != builtins.end())
       std::cout << s << " is a shell builtin\n";
       else std::cout << "invalid_command: not found\n"; 
 
