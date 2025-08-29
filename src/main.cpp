7f67ef7b834972d823ad43c38622ff09cb742f50
Tareq Alazzam <tareqalazzam6@gmail.com>
2025-08-29 04:48:17 +0300

    repl

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,8 @@ int main() {
   std::string input;
   
   while(true) {
-    if(!std::getline(std::cin, input))break; 
     std::cout << "$ ";
-    std::getline(std::cin, input); 
+    if(!std::getline(std::cin, input))break; 
     std::cout << input << ": command not found" << std::endl; 
   }
 }
