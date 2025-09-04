48bd2749e5464f3dc333514ef92b23d8bb51851d
Tareq Alazzam <tareqalazzam6@gmail.com>
2025-09-04 06:18:17 +0300

    appendStdout1

--- source/redirection.cpp
+++ source/redirection.cpp
@@ -98,7 +98,7 @@ Shell::parseRedirection(const std::vector<std::string>& tokens) {
             stdoutFile.append(tokens[i+1]); 
             redirectIndex = static_cast<int>(i); 
         } else {
-            std::cerr << "Syntax error near unexpected token `newline`\n"; 
+            std::cerr << "Syntax error near unexpected token `newline` << we are hear>> \n"; 
             return {{}, {"", ""}}; 
         }
     }
